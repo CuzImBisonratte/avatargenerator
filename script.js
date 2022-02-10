5ecf258229ffb6a6c13038d6b9cb40c70cee2d52
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-02-10 19:31:02 +0100

    added linkage

---
+++ script.js
@@ -0,0 +1,19 @@
+function openSimple() {
+    document.body.style.filter = "blur(5vh)";
+
+    // Redirect to simple.html
+    window.setTimeout(function() {
+        location.href = "simple.html";
+
+    }, 200);
+}
+
+function openCustom() {
+    document.body.style.filter = "blur(5vh)";
+
+    // Redirect to custom.html
+    window.setTimeout(function() {
+        location.href = "custom.html";
+
+    }, 200);
+}
